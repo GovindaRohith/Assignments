87b6c2a73caa4dd61b10e05edb969c0e39505625
GovindaRohith <102508383+GovindaRohith@users.noreply.github.com>
2022-05-08 11:21:46 +0530

    Add files via upload

---
+++ Assignment 7/code/calc.c
@@ -0,0 +1,15 @@
+/*
+Code by Govinda Rohith Y
+CS21BTECH11062
+for NCERT class 12 Exercise 13.1 prob 1
+*/
+#include<stdio.h>
+int main()
+{
+    double pa=0.6;
+    double pb=0.2;
+    //pa denotes P(A)
+    //pb denotes P(B)
+    printf("P(A intersection B)=%lf",pa*pb);
+    return 0;
+}
