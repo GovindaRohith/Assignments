e111a1b5ce6ac87586d98418a3a45734db07d16c
GovindaRohith <102508383+GovindaRohith@users.noreply.github.com>
2022-06-19 19:26:32 +0530

    Update varcal.py

--- Assignment 13/codes/varcal.py
+++ Assignment 13/codes/varcal.py
@@ -1,1 +1,9 @@
-
+import matplotlib.pyplot as plt
+import numpy as np
+import sympy as sy
+  
+def f(x):
+    return 2*(10-x)*(64+10*sy.exp(-2*x))
+  
+x = sy.Symbol("x")
+print(sy.integrate(f(x), (x, 0, 10))-80*80)
